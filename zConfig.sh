f7055a2fcab9c9d10a4a90fc43e36a687da063b4
Viacheslav Pradchuk <viacheslav.pradchuk@inoxoft.com>
2016-11-03 11:43:36 +0200

    Test

--- zConfig.sh
+++ zConfig.sh
@@ -3,5 +3,5 @@ TargetBranch='refs/remotes/origin/master'
 CiBranch='CI'
 RepoVersionFile="version_repo.res"
 ServerVersionFile="version_iis.res"
-MSBuildPath=""
-PublishProfileName=""
+MSBuildPath="C:/Program Files (x86)/MSBuild/14.0/Bin/MSBuild.exe"
+PublishProfileName="LocalDeploy"
